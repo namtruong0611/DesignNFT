712fcce294fee537f455d6580a1de59bac5adddb
Namtruong <118741740+namtruong0611@users.noreply.github.com>
2024-03-24 10:16:33 +0700

    loginjs

---
+++ Screen/Login.js
@@ -0,0 +1,35 @@
+
+  function showRegisterForm() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "block";
+    document.getElementById("forgotPasswordForm").style.display = "none";
+  }
+
+  function showForgotPasswordForm() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("registerForm").style.display = "none";
+    document.getElementById("forgotPasswordForm").style.display = "block";
+  }
+
+  function HomeScreen(event) {
+    // Ngăn chặn hành động mặc định của form
+    event.preventDefault();
+
+    // Thực hiện kiểm tra thông tin đăng nhập ở đây
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Giả sử thông tin đăng nhập hợp lệ
+    if (username === "admin" && password === "admin") {
+        // Chuyển hướng đến trang homeScreen.html
+        window.location.href = "HomeScreen.html";
+    } else {
+        alert("Invalid username or password!");
+    }
+}
+function Login(event){
+    window.location.href = "HomeScreen.html";
+}
+
+
+
